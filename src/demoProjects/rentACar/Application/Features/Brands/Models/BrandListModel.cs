320e0a9db10f18d24bb14b5d4b70269004a0f007
Burak <brkggs@gmail.com>
2022-10-03 23:58:21 +0300

    get queries, command validation

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Models/BrandListModel.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Brands.Dtos;
+using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Models
+{
+    //class isminin model olmasının sebebi db den aldığımız bilgileri dto olarak dönmenin yanı sıra sayfalama bilgisi de döndüğümüz içindir
+    public class BrandListModel:BasePageableModel
+    {
+        public IList<BrandListDto> Items { get; set; }
+    }
+}
