82899a707ddf0a78bacf424573cdd49e2faaa115
Burak <brkggs@gmail.com>
2022-10-08 23:12:01 +0300

    models operations, dynamic search, jwt alt yapısı - registration, accestoken, refreshtoken

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery]PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest};
+            ModelListModelByDynamic reault = await Mediator.Send(getListModelQuery);
+            return Ok(reault);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListModelByDynamicQuery getListModelByDynamicQuery = new GetListModelByDynamicQuery{ PageRequest = pageRequest, Dynamic = dynamic };
+            ModelListModelByDynamic reault = await Mediator.Send(getListModelByDynamicQuery);
+            return Ok(reault);
+        }
+    }
+}
